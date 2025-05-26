b132eb83441739671556b287ee8aa8fe1570034e
Md Solyman Hossen <105260985+solymanHossen@users.noreply.github.com>
2025-05-26 23:44:03 +0600

    created product slug

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,5 +1,6 @@
 import mongoose, { Document, Schema } from 'mongoose';
 import {IReview} from "./review.model";
+import slugify from "slugify";
  export interface IGalleryImage {
     _id?: any;
     url?: string | undefined;
@@ -8,6 +9,7 @@ import {IReview} from "./review.model";
 
 export interface IProduct extends Document {
     name: string;
+    slug: string;
     description: string;
     htmlDescription: string;
     price: number;
@@ -26,6 +28,7 @@ const GalleryImageSchema = new Schema({
 })
 const productSchema = new Schema<IProduct>({
     name: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
     description: { type: String, required: true },
     htmlDescription: { type: String, required: true },
     price: { type: Number, required: true },
@@ -38,5 +41,16 @@ const productSchema = new Schema<IProduct>({
     reviewCount: { type: Number, default: 0 },
     stock:{ type: Number,required: true },
 }, { timestamps: true });
+productSchema.pre('validate', async function (next) {
+    if (!this.isModified('name')) return next();
+    const baseSlug = slugify(this.name, { lower: true, strict: true });
+    let slug = baseSlug;
+    let count = 1;
+    while (await Product.exists({ slug })) {
+        slug = `${baseSlug}-${count++}`;
+    }
+    this.slug = slug;
+    next();
+});
 
 export const Product = mongoose.model<IProduct>('Product', productSchema);
